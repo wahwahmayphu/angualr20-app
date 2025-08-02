6a827fb2906dfb08111a5884c78518812935d356
wahwahmayphu <wahwahmayphu1997@gmail.com>
2025-08-02 23:27:32 +0630

    add control flow statement @else if and @for

--- angular20Tutorial/src/app/components/control-flow/control-flow.ts
+++ angular20Tutorial/src/app/components/control-flow/control-flow.ts
@@ -1,14 +1,26 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-control-flow',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './control-flow.html',
   styleUrl: './control-flow.css'
 })
 export class ControlFlow {
 
   isParaVisible: boolean = true;
+
+  startMonthName: string = "feb";
+
+  cityList: string [] = ["Yangon", "Singapore", "Bankok", "Mawlamyine"]
+
+  studentList: any [] = [
+    {name:'PMT', city: 'Singapore', isActive: false},
+    {name:'MP', city: 'Yangon', isActive: false},
+    {name:'SS', city: 'Mawlamyine', isActive: true},
+    {name:'SMN', city: 'Bankok', isActive: false}
+  ]
 
   showP() {
     this.isParaVisible = true;
