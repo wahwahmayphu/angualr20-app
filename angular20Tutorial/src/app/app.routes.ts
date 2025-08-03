91b803b60a9b4484a657543374cb3c77c2f5bedd
wahwahmayphu <wahwahmayphu1997@gmail.com>
2025-08-03 21:12:42 +0630

    create routes, router Outlet and routerLink

--- angular20Tutorial/src/app/app.routes.ts
+++ angular20Tutorial/src/app/app.routes.ts
@@ -1,3 +1,26 @@
 import { Routes } from '@angular/router';
+import { Admin } from './components/admin/admin';
+import { ControlFlow } from './components/control-flow/control-flow';
+import { DataBinding } from './components/data-binding/data-binding';
+import { SignalEx } from './components/signal-ex/signal-ex';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+   {
+      path:'admin',
+      component: Admin
+   },
+
+   {
+      path: 'control-flow-statement',
+      component: ControlFlow
+   },
+   {
+      path: 'data-binding',
+      component: DataBinding
+   },
+   {
+      path:'signal',
+      component: SignalEx
+   }
+ 
+];
--- angular20Tutorial/src/app/app.ts
+++ angular20Tutorial/src/app/app.ts
@@ -1,5 +1,5 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 import { Admin } from './components/admin/admin';
 import { User } from './components/user/user';
 import { DataBinding } from './components/data-binding/data-binding';
@@ -10,7 +10,7 @@ import { ControlFlow } from './components/control-flow/control-flow';
   selector: 'app-root',
   // imports: [Admin,User, DataBinding],
  //imports: [SignalEx],
-  imports:[ControlFlow],
+  imports:[ControlFlow, RouterLink, RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
